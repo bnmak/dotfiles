098078cadc4d1557556008e6a2fbf089f704f80d
Brian Myers <brian.myers.alaska@gmail.com>
2015-08-05 19:12:36 -0800

    Modified to include dircolors

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc Xresources tmux.conf"    # list of files/folders to symlink in homedir
+files="vimrc vim zshrc Xresources tmux.conf dircolors"    # list of files/folders to symlink in homedir
 
 ##########
 
